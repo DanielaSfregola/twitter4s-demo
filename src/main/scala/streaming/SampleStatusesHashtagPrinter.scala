59ae774be415713ff1ebc701823f50f38e7e9fe2
Daniela Sfregola <daniela.sfregola@gmail.com>
2017-10-27 03:47:29 +0200

    Minor Refactoring non associated to this PR

--- src/main/scala/streaming/SampleStatusesHashtagPrinter.scala
+++ src/main/scala/streaming/SampleStatusesHashtagPrinter.scala
@@ -8,20 +8,16 @@ object SampleStatusesHashtagPrinter extends App {
 
   val client = TwitterStreamingClient()
 
-  def printHashtags(tweet: Tweet) = tweet.entities match {
-    case None => ()
-    case Some(e) => e.hashtags.foreach { h =>
+  def printHashtags(tweet: Tweet) = tweet.entities.map { e =>
+    e.hashtags.foreach { h =>
       println(h.text)
     }
   }
 
-  def filterTweetByHashtag(tweet: Tweet, myAwesomeHashtag: String): Option[Tweet] = tweet.entities match {
-    case None => None
-    case Some(e) =>
+  def filterTweetByHashtag(tweet: Tweet, myAwesomeHashtag: String): Option[Tweet] = tweet.entities.map { e =>
       val hashtagTexts = e.hashtags.map(_.text.toUpperCase)
-      if (hashtagTexts.contains(myAwesomeHashtag.toUpperCase)) {
-        Some(tweet)
-      } else None
+      if (hashtagTexts.contains(myAwesomeHashtag.toUpperCase)) Some(tweet)
+      else None
   }
 
   client.firehoseStatuses() {
