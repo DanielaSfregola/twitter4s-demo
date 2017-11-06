7c28f35e9da249db15aeb26b0f89dc1564221f63
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2017-11-06 11:10:10 +0100

    remove unnecessary value and use foreach

--- src/main/scala/rest/FollowersRegisteredTheSameYear.scala
+++ src/main/scala/rest/FollowersRegisteredTheSameYear.scala
@@ -9,34 +9,23 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object FollowersRegisteredTheSameYear extends App {
+  def getSameRegistrationYearFollowers(creationDate: Date, followers: Set[User]): Set[User] = {
+    followers.filter(_.created_at.getYear == creationDate.getYear)
+  }
 
-	def getSameRegistrationYearFollowers(creationDate: Date, followers: Set[User]): Set[User] = {
-		followers.filter(_.created_at.getYear == creationDate.getYear)
-	}
+  def printUserRegistration(follower: User): Unit = {
+    println(s"${follower.screen_name} created at ${follower.created_at}")
+  }
 
-	def printUserRegistration(follower: User): Unit = {
-		println(s"${follower.screen_name} created at ${follower.created_at}")
-	}
+  // TODO - Make sure to define your consumer and access tokens!
+  val restClient     = TwitterRestClient()
+  val userScreenName = "ScalaIO_FR"
 
-	// TODO - Make sure to define your consumer and access tokens!
-	val restClient = TwitterRestClient()
+  val sameYearFollowers: Future[Set[User]] = for {
+    user      <- restClient.user(userScreenName)
+    followers <- restClient.followersForUser(user.data.screen_name).map(_.data.users)
+  } yield getSameRegistrationYearFollowers(user.data.created_at, followers.toSet)
 
-	val userScreenName = "ScalaIO_FR"
-
-	val userEntity: Future[RatedData[User]] = restClient.user(userScreenName)
-
-
-	val sameYearFollowers: Future[Set[User]] = for {
-		user <- userEntity
-		followers <- restClient.followersForUser(user.data.screen_name).map(_.data.users)
-	} yield getSameRegistrationYearFollowers(user.data.created_at, followers.toSet)
-
-	userEntity.map(rd => {
-		printUserRegistration(rd.data)
-		println("--")
-	})
-
-	sameYearFollowers.map(_.foreach(follower => printUserRegistration(follower)))
-
+  sameYearFollowers.foreach(_.foreach(follower => printUserRegistration(follower)))
 
 }
